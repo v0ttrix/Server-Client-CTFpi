6919e6785d4a859954bc0c0a94f679278783f704
Ahmed Somaan <ahmed_somaan@hotmail.com>
2026-02-10 21:36:48 -0500

    created folder structure for different pages

--- Frontend/CTF/src/App.jsx
+++ Frontend/CTF/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Home />
     </>
   );
 }
