fc040fb1a4b1ebc7ed8ddf53e30a3d7e5698d1ba
Ahmed Somaan <ahmedsomaan7@gmail.com>
2026-02-11 11:04:44 -0500

    created structure for page navigation using react router

--- Frontend/CTF/src/App.jsx
+++ Frontend/CTF/src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Challenges from "./pages/Challenges";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/challenges" element={<Challenges />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
